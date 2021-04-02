de06b2549d60ca661a0fe82672090f29d7759dc0
Caio César Beleza <caio.almeidabeleza@gmail.com>
2021-04-01 21:38:35 -0300

    Initiating parse to xml on SequenceDiagram

--- Sequencia/App/diagrama_sequencia.py
+++ Sequencia/App/diagrama_sequencia.py
@@ -1,10 +1,25 @@
+import xml.etree.cElementTree as ET
+
+
 class DiagramaSequencia:
-    def __init__(self, nome: str, condicao_guarda: bool):
-        self.nome = nome
-        self.condicao_guarda = condicao_guarda
+    def __init__(self, nome: str, condicao_guarda: str):
+       self.nome = nome
+       self.condicao_guarda = condicao_guarda
 
     def CadastrarDiagramaSequencia(self):
 
+      
+        SequenceDiagram = ET.Element("SequenceDiagram")
+        Lifelines = ET.SubElement(SequenceDiagram, "Lifelines")
+
+        ET.SubElement(Lifelines, "Lifeline", name='Lifeline 1')
+        #ET.SubElement(SequenceDiagram, "SequenceDiagram", name='Lifeline 1', condicao_guarda=self.condicao_guarda)
+
+        tree = ET.ElementTree(SequenceDiagram)
+        tree.write("filename.xml")
+
+diagrama1 = DiagramaSequencia("Teste", "condicao")
+diagrama1.CadastrarDiagramaSequencia()
 
 
 
