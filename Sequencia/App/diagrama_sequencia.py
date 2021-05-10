91a19d7f6e4475e14b681bb5a33ef8692f027d69
Caiocbeleza <caio.almeidabeleza@gmail.com>
2021-05-10 16:42:05 -0300

    Substituir método por objeto-método
    Essa refatoração foi aplicada no método que era da classe DiagramaSequencia(). Os atributos deixados na classe DiagramaSequencia() correspondem as variáveis temporárias que existiam no método da classe. Foi criada uma nova classe CriarDiagramaSequencia(), a partir do método que existia na classe de origem. CriarDiagramaSequencia possui uma referência a classe DiagramaSequencia para acessar os atributos dessa classe e o método único que havia na classe DiagramaSequencia foi dividido e deu origem aos novos métodos criar_lifelines(), criar_mensagens() e criar_fragmentos() contidos na nova classe CriarDiagramaSequencia.

--- Sequencia/App/diagrama_sequencia.py
+++ Sequencia/App/diagrama_sequencia.py
@@ -1,30 +1,41 @@
 import xml.etree.cElementTree as ET
 
+
 class DiagramaSequencia():
     def __init__(self, nome_diagrama = "", condicao_guarda="", lifelines=[], mensagens=[], fragmentos=[]):
-        SequenceDiagram = ET.Element("SequenceDiagram", name=nome_diagrama)
-        SequenceDiagramElements = ET.SubElement(SequenceDiagram, "SequenceDiagramElements")
-        
-        return Lifeline(self.lifelines)
-        return Mensagem(self.mensagens)
-        return Fragmentos(self.Fragmentos)
-                   
-        tree = ET.ElementTree(SequenceDiagram)
-        tree.write("diagrama_sequencia.xml")
+            self.nome_diagrama = nome_diagrama
+            self.condicao_guarda = condicao_guarda
+            self.lifelines = lifelines
+            self.mensagens = mensagens
+            self.fragmentos = fragmentos
 
-class Lifeline():
-    def __init__(self, lifelines=[]):
-        for lifeline in self.lifelines:
-            ET.SubElement(SequenceDiagramElements, "Lifeline", name=lifeline)
-class Mensagem():
-    def __init__(self, mensagens=[]):
-        for mensagem in self.mensagens:
-            ET.SubElement(SequenceDiagramElements, "Message", name=mensagem['name'], prob=str(mensagem['prob']),source=mensagem['source'], target=mensagem['target'])      
-class Fragmentos():
-   def __init__(self, fragmentos=[]):
-        for fragmento_opcional in self.fragmentos:
-            ET.SubElement(SequenceDiagramElements, "Fragmento Opcional", name=fragmento_opcional['nome'], condicao_guarda=str(fragmento_opcional['condicao_guarda']),representedBy=fragmento_opcional['representedBy'])
-           
+class CriarDiagramaSequencia(DiagramaSequencia):
+        def __init__(self, nome_diagrama, condicao_guarda, lifelines, mensagens, fragmentos):
+                
+                SequenceDiagram = ET.Element("SequenceDiagram", name=nome_diagrama)
+                SequenceDiagramElements = ET.SubElement(SequenceDiagram, "SequenceDiagramElements")
+
+                def criar_lifelines():
+                        for lifeline in lifelines:
+                                ET.SubElement(SequenceDiagramElements, "Lifeline", name=lifeline)
+                                tree = ET.ElementTree(SequenceDiagram)
+                                tree.write("diagrama_sequencia.xml")
+
+                def criar_mensagens():
+                        for mensagem in mensagens:
+                                ET.SubElement(SequenceDiagramElements, "Message", name=mensagem['name'], prob=str(mensagem['prob']),source=mensagem['source'], target=mensagem['target'])
+                                tree = ET.ElementTree(SequenceDiagram)
+                                tree.write("diagrama_sequencia.xml")
+                
+                def criar_fragmentos():
+                        for fragmento_opcional in fragmentos:
+                                ET.SubElement(SequenceDiagramElements, "Fragmento Opcional", name=fragmento_opcional['nome'], condicao_guarda=str(fragmento_opcional['condicao_guarda']),representedBy=fragmento_opcional['representedBy'])
+                                tree = ET.ElementTree(SequenceDiagram)
+                                tree.write("diagrama_sequencia.xml")
+
+                adicionar_lifelines = criar_lifelines()
+                adicionar_mensagens = criar_mensagens()
+                adicionar_fragmentos = criar_fragmentos()
 
 # diagrama1 = DiagramaSequencia("Teste", "condicao")
 # diagrama1.CadastrarDiagramaSequencia()
--- Sequencia/main.py
+++ Sequencia/main.py
@@ -1,4 +1,4 @@
-from App.diagrama_sequencia import DiagramaSequencia
+from App.diagrama_sequencia import CriarDiagramaSequencia
 from App.mensagens import AdicionarMensagem, MensagemAssincrona, MensagemSincrona, MensagemResposta
 from App.lifeline import Lifeline
 from App.fragments import Fragments
@@ -6,9 +6,9 @@ from App.fragments import Fragments
 if __name__ == '__main__':
     lifeline1 = Lifeline('Lifeline1')
     lifeline2 = Lifeline('Lifeline2')
-    mensagem_sincrona = AdicionarMensagem('Mensagem Sincrona', 0.5, 'Source', 'Terget')
-    mensagem_assincrona = AdicionarMensagem('Mensagem Assincrona', 0.8, 'Lifeline1', 'Lifeline2')
-    resposta = AdicionarMensagem('Resposta', 0.6, 'Lifeline 2', 'Lifeline 1')
+    mensagem_sincrona = AdicionarMensagem('Mensagem Sincrona', 0.32, 'Source', 'Terget')
+    mensagem_assincrona = AdicionarMensagem('Mensagem Assincrona', 0.23, 'Lifeline1', 'Lifeline2')
+    resposta = AdicionarMensagem('Resposta', 0.57, 'Lifeline 2', 'Lifeline 1')
     fragment = Fragments('Fragmento Opcional', True, 'Nome do diagrama')
 
-    diagrama = DiagramaSequencia('Nome do diagrama', 'condicao', lifeline1.arrayLifeline, mensagem_sincrona.arrayMensagem, fragment.arrayFragments)
+    diagrama = CriarDiagramaSequencia('Nome do diagrama', 'condicao', lifeline1.arrayLifeline, mensagem_sincrona.arrayMensagem, fragment.arrayFragments)
